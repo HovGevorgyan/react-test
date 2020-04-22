72b0597b44e903e3257037a8f9ccbd410d7418ed
hovhannessteadfast <hovhannes@steadfast.tech>
2020-04-22 17:04:39 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Headroom from 'react-headroom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import classes from './App.module.css';
+
+class App extends Component {
+
+    state = {
+        isDesktop: false
+    }
+
+    handleWindowResize = () => {
+        this.setState({ isDesktop: window.innerWidth >= 375 });
+    }
+
+    componentDidMount() {
+        this.handleWindowResize();
+    }
+
+    render() {
+        return (
+            <div className={classes.App}>
+                <Headroom disable={this.state.isDesktop}>
+                    <div className={classes.Nav}>
+                        {this.state.isDesktop ? 'HEADER' : 'Changed Header'}
+                    </div>
+                </Headroom>
+                <div style={{height: '1500px'}}>
+
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
